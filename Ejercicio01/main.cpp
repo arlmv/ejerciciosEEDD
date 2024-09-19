1124b30d043da8820dfbdd436e9cfb8e0f0e5005
arlmv <149476006+arlmv@users.noreply.github.com>
2024-09-19 21:55:21 +0200

    Initial commit

---
+++ Ejercicio01/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+
+int main(int argc, char **argv)
+{
+    int x;
+    std::cout << "Escriba un número: ";
+    std::cin >> x;
+    std::cout << "El número introducido es: " << x << "\n";
+    return 0;
+}
